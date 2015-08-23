a0e944f78e7712e0fa48d3e87515bf18506e3c28
Alex Milanov <alex@i4web.biz>
2015-08-23 12:52:27 +0300

    3-set-up-initial-tools
    https://trello.com/c/yf6yy9Ab/3-set-up-initial-tools
    
    - model strcture from Qoolejs
    - iblokz.Resource
    - CumufluxApp

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+
+var CumufluxApp = function(){
+
+}
+
+CumufluxApp.prototype.init = function(){
+	console.log("Hello World");
+}
+
+
+var app = new CumufluxApp();
+
+app.init();
---
+++ src/js/iblokz/resource.js
@@ -0,0 +1,49 @@
+"use strict";
+
+if(typeof iblokz === "undefined"){ var iblokz = {}; }
+
+iblokz.Resource = function(url) {
+  this.url = url;
+}
+
+iblokz.Resource.prototype.query = function(queryParams) {
+
+	return Q($.ajax({
+	    url: this.url, // api endpoint
+	    method: "get",          // method
+	    data: queryParams,
+	    dataType: "json"        // data type
+	}));
+}
+
+iblokz.Resource.prototype.create = function(data) {
+	return Q($.ajax({
+	    url: this.url, // api endpoint
+	    method: "post",          // method
+	    data: data,
+	    dataType: "json"        // data type
+	}));
+}
+
+// api/students/:id -> GET, PUT, DELETE
+
+iblokz.Resource.prototype.view = function(id) {
+	return Q($.get(this.url+"/"+id));
+}
+
+iblokz.Resource.prototype.update = function(id, data) {
+	return Q($.ajax({
+	    url: this.url+"/"+id, // api endpoint
+	    method: "put",          // method
+	    data: data,
+	    dataType: "json"        // data type
+	}));
+}
+
+iblokz.Resource.prototype.delete = function(id) {
+	return Q($.ajax({
+	    url: this.url+"/"+id, // api endpoint
+	    method: "delete",
+	    dataType: "json"        // data type
+	}));
+}
