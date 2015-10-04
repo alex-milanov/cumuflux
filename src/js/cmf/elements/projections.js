b21ef176a319115a8f83ffd845a69c11661c55ae
Alex Milanov <alex@i4web.biz>
2015-10-04 17:46:07 +0300

    18-issue-projection-does-not-work-properly
    https://trello.com/c/JP0nZlUJ/18-issue-projection-does-not-work-properly

--- src/js/cmf/elements/projections.js
+++ src/js/cmf/elements/projections.js
@@ -20,36 +20,54 @@ cmf.elements.Projections.prototype.init = function(){
 	var resource = new cmf.resources.Transactions("/api/transactions");
 	return resource.query().then(function(result){
 
-		console.log(result);
-
 		var nextWeekAmount = 0;
+		var thisMonthAmount = 0;
 		var nextMonthAmount = 0;
 
+		var nextWeekStart = moment().startOf('isoweek').add(1, 'weeks');
+		var nextWeekEnd = moment().endOf('isoweek').add(1, 'weeks');
+
+		var thisMonth =  {
+			start: moment().startOf("month"),
+			end: moment().endOf("month")
+		}
+
+		var nextMonth =  {
+			start: thisMonth.start.clone().add(1, 'month'),
+			end: thisMonth.end.clone().add(1, 'month')
+		}
 
 		result.list.forEach(function(item){
 			switch(item.occuring){
 				case "weekly":
-					if(item.next >= moment().day(1).add(1, 'weeks') && item.next <= moment().day(7).add(1, 'weeks')){
+					if(item.next >= nextWeekStart && item.next <= nextWeekEnd){
 						nextWeekAmount += item.amount;
 					}
+					thisMonthAmount += item.amount*4;
 					nextMonthAmount += item.amount*4;
 					break;
 				case "monthly":
 					if(item.next) {
-						if(item.next >= moment().day(1).add(1, 'weeks').startOf('day') && item.next <= moment().day(7).add(1, 'weeks').startOf('day')){
+						if(item.next >= nextWeekStart && item.next <= nextWeekEnd){
 							nextWeekAmount += item.amount;
 						}
-						if(item.next >= moment().set('date',1).add(1, 'months').startOf('day') && item.next < moment().set('date',1).add(2, 'months').startOf('day')){
+						if(item.occuredAt <= thisMonth.end){
+							thisMonthAmount += item.amount;
+						}
+						if(item.occuredAt <= nextMonth.end){
 							nextMonthAmount += item.amount;
 						}
 					}
 					break;
 				case "once":
 					if(item.occuredAt) {
-						if(item.occuredAt >= moment().day(1).add(1, 'weeks').startOf('day') && item.occuredAt <= moment().day(7).add(1, 'weeks').startOf('day')){
+						if(item.occuredAt  >= nextWeekStart && item.occuredAt <= nextWeekEnd){
 							nextWeekAmount += item.amount;
 						}
-						if(item.occuredAt >= moment().set('date',1).add(1, 'months').startOf('day') && item.occuredAt < moment().set('date',1).add(2, 'months').startOf('day')){
+						if(item.occuredAt >= thisMonth.start && item.occuredAt < thisMonth.end){
+							thisMonthAmount += item.amount;
+						}
+						if(item.occuredAt >= nextMonth.start && item.occuredAt < nextMonth.end){
 							nextMonthAmount += item.amount;
 						}
 					}
@@ -57,6 +75,7 @@ cmf.elements.Projections.prototype.init = function(){
 			}
 
 			$(projections._dom).find(".next-week").text(nextWeekAmount);
+			$(projections._dom).find(".this-month").text(thisMonthAmount);
 			$(projections._dom).find(".next-month").text(nextMonthAmount);
 		});
 	});
